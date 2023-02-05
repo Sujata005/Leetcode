a9eda3a2a9c40be28e13be4ff13066ead57bf74d
Sujata <112793420+Sujata005@users.noreply.github.com>
2023-02-05 22:16:00 +0530

    Medium

---
+++ All C submissions/33. Search in Rotated Sorted Array.c
@@ -0,0 +1,16 @@
+int search(int* nums, int numsSize, int target){
+    int low =0, high=numsSize-1;
+    while(low<=high){
+        int mid=low+(high-low)/2;
+        if(nums[mid]==target) return mid;
+        if(nums[low]<=nums[mid]){
+            if(target>=nums[low]&&target<=nums[mid]) high=mid-1;
+            else low=mid+1;
+        }
+        else{
+            if(target>=nums[mid]&&target<=nums[high]) low=mid+1;
+            else high=mid-1;
+        }
+    }
+    return -1;
+}
