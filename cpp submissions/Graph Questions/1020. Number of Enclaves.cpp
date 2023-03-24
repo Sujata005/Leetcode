fbdde4ca0535f87d3a57b86a4c6d905729ff5ddf
Sujata <112793420+Sujata005@users.noreply.github.com>
2023-03-24 19:42:30 +0530

    Medium

---
+++ cpp submissions/Graph Questions/1020. Number of Enclaves.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int numEnclaves(vector<vector<int>>& grid) {
+        int n=grid.size(), m=grid[0].size();
+        vector<vector<int>> vis(n,vector<int>(m,0));
+        queue<pair<int,int>> q;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==0 || j==0 || i==n-1 || j==m-1){
+                    if(grid[i][j]==1){
+                        q.push({i,j});
+                        vis[i][j]=1;
+                    }
+                }
+            }
+        }
+        int xrow[]={-1,0,1,0};
+        int xcol[]={0,1,0,-1};
+        while(!q.empty()){
+            int row=q.front().first;
+            int col=q.front().second;
+            q.pop();
+            for(int i=0;i<4;i++){
+                int nrow=row+xrow[i];
+                int ncol=col+xcol[i];
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol]==0 && grid[nrow][ncol]==1){
+                    vis[nrow][ncol]=1;
+                    q.push({nrow,ncol});
+                }
+            }
+        }
+        int land=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1 && vis[i][j]==0) land++;
+            }
+        }
+        return land;
+    }
+};
