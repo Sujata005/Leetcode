eb02bf53ca6c1f93a2be22e6359c0a32a9e1c4dd
Sujata <112793420+Sujata005@users.noreply.github.com>
2023-03-24 19:41:25 +0530

    Medium

---
+++ cpp submissions/Graph Questions/542. 01 Matrix.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int n=mat.size(), m=mat[0].size();
+        vector<vector<int>> vis(n,vector<int>(m,0));
+        vector<vector<int>> ans(n,vector<int>(m,0));
+        queue<pair<pair<int,int>,int>> q;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j]==0){
+                    q.push({{i,j},0});
+                    vis[i][j]=1;
+                }
+                else vis[i][j]=0;
+            }
+        }
+        int xrow[]={-1,0,1,0};
+        int xcol[]={0,1,0,-1};
+        while(!q.empty()){
+            int row=q.front().first.first;
+            int col=q.front().first.second;
+            int step=q.front().second;
+            q.pop();
+            ans[row][col]=step;
+            for(int i=0;i<4;i++){
+                int nrow=row+xrow[i];
+                int ncol=col+xcol[i];
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol]==0){
+                    vis[nrow][ncol]=1;
+                    q.push({{nrow,ncol},step+1});
+                }
+            }
+        }
+        return ans;
+    }
+};
