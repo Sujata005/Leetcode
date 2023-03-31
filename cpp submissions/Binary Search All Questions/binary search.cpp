e6532dc66c43e0158082ccbc4838575321633ae9
Sujata <112793420+Sujata005@users.noreply.github.com>
2023-03-31 21:38:28 +0530

    Easy

--- cpp submissions/Binary Search All Questions/binary search.cpp
+++ cpp submissions/Binary Search All Questions/binary search.cpp
@@ -1,1 +1,13 @@
-
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l=0,m=0,h=nums.size()-1;
+        while(l<=h){
+        m=l+(h-l)/2;
+        if(nums[m]==target) return m;
+        else if(nums[m]>target) h=m-1;
+        else l=m+1;
+    }
+    return -1;
+    }
+};
