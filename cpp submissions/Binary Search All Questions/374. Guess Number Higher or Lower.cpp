aefc83f46e9f3123fcd125ed5481bda65f96d493
Sujata <112793420+Sujata005@users.noreply.github.com>
2023-03-31 21:40:59 +0530

    Easy

---
+++ cpp submissions/Binary Search All Questions/374. Guess Number Higher or Lower.cpp
@@ -0,0 +1,24 @@
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+class Solution {
+public:
+    int guessNumber(int n) {
+        int low = 1, high = n;
+        int mid;
+        while (low <= high) {
+          mid = low + (high - low) / 2;
+          if (guess(mid) == 0) return mid;
+          else if (guess(mid) < 0) high = mid - 1;
+          else low = mid + 1;
+      }
+               
+      return mid;
+    }
+};
